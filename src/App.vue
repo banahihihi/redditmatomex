58264e1d881367f07a3aa0ed1a7ae120346583d3
banahihihi <songxiafengzhi@gmail.com>
2024-08-02 14:28:43 +0900

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { ref } from "vue";
+const res = ref("res初期値");
+const url = ref("url初期値");
 
+function getUrl() {
+  res.value = url.value;
+}
 </script>
 
 <template>
-  <p>ap</p>
+  <input type="text" v-bind:value="url" />
+  <button @click="getUrl">Click</button>
+  <p>{{ res }}</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
