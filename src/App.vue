78bb5902c576f7b947d055123265416d2f21ff7d
banahihihi <songxiafengzhi@gmail.com>
2024-08-04 21:58:14 +0900

    GAS移行

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,97 @@
 <script setup>
+import axios from 'axios';
 import { ref } from "vue";
-const res = ref("res初期値");
-const url = ref("url初期値");
 
-function getUrl() {
-  res.value = url.value;
+const res = ref("res初期値");
+const input = ref("https://www.reddit.com/r/OsakaTravel/comments/1e8na56/is_this_area_safe/");
+
+async function getUrl() {
+  let url = input.value;
+
+  let out = await matome(url);
+
+  let values = [[out.title.body, out.title.bodyjp],[out.title.selftext, out.title.selftestjp]];
+  out.comments.forEach((comment) => {
+    values.push([comment.body,comment.bodyjp])
+  })
+
+  res.value = values;
 }
+
+async function matome(url) {
+  
+  let jsonUrl = url + ".json";
+
+  let out;
+
+  await axios
+  .get(jsonUrl)
+  .then((response) => {
+
+    let body = response.data;
+
+    //タイトルの取得、selfは投稿者のコメント
+    let title = {
+      body: body[0].data.children[0].data.title,
+      bodyjp : "LanguageApp.translate(body[0].data.children[0].data.title, `en`, `ja`)",
+      selftext : body[0].data.children[0].data.selftext,
+      selftestjp : "LanguageApp.translate(body[0].data.children[0].data.selftext, `en`, `ja`)",
+      author: body[0].data.children[0].data.author,
+      id: body[0].data.children[0].data.id,
+      url: body[0].data.children[0].data.url,
+    };
+
+    let comments = [];//コメント
+
+    //コメントbodyとrepliesを取得
+    body[1].data.children.forEach((child) => {
+      extractCommentsAndReplies(child, comments);
+    });
+
+    out = {
+      title: title,
+      comments: comments
+    }
+
+  })
+  .catch(error => console.error('There was a problem with your axios operation:', error));  
+
+  return out;
+
+}
+
+const extractCommentsAndReplies = (child, comments) => {
+  //Utilities.sleep(100);
+
+  comments.push({
+    body: child.data.body,
+    bodyjp : "LanguageApp.translate(child.data.body, `en`, `ja`)",
+    author: child.data.author,
+    id: child.data.id,
+    parent_id: child.data.parent_id,
+  });
+
+  if(child.data.replies != ""){
+    //再帰関数で処理
+    child.data.replies.data.children.forEach((child) => {
+      extractCommentsAndReplies(child, comments);
+    });
+  }
+}
+
 </script>
 
 <template>
-  <input type="text" v-bind:value="url" />
-  <button @click="getUrl">Click</button>
-  <p>{{ res }}</p>
+  <div class="template">
+    <p>以下vue単一コンポーネント</p>
+    <input type="text" v-model.lazy="input" />
+    <button @click="getUrl">Click</button>
+    <p>{{ res }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .template{
+    background-color: aquamarine;
+  }
+</style>
