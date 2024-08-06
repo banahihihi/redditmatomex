558b1d89af50f3063e91768f0846b8b425d5a53d
banahihihi <songxiafengzhi@gmail.com>
2024-08-06 15:05:37 +0900

    配列をrefにした

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,69 @@
 <script setup>
-import axios from 'axios';
+import axios from "axios";
 import { ref } from "vue";
 
 const res = ref("res初期値");
-const input = ref("https://www.reddit.com/r/OsakaTravel/comments/1e8na56/is_this_area_safe/");
+const input = ref(
+  "https://www.reddit.com/r/OsakaTravel/comments/1e8na56/is_this_area_safe/"
+);
+const items = ref([]);
 
 async function getUrl() {
   let url = input.value;
 
   let out = await matome(url);
 
-  let values = [[out.title.body, out.title.bodyjp],[out.title.selftext, out.title.selftestjp]];
+  items.value.push(
+    [out.title.body, out.title.bodyjp],
+    [out.title.selftext, out.title.selftestjp]
+  );
   out.comments.forEach((comment) => {
-    values.push([comment.body,comment.bodyjp])
-  })
+    items.value.push([comment.body, comment.bodyjp]);
+  });
 
-  res.value = values;
+  res.value = items.value;
 }
 
 async function matome(url) {
-  
   let jsonUrl = url + ".json";
 
   let out;
 
   await axios
-  .get(jsonUrl)
-  .then((response) => {
+    .get(jsonUrl)
+    .then((response) => {
+      let body = response.data;
 
-    let body = response.data;
+      //タイトルの取得、selfは投稿者のコメント
+      let title = {
+        body: body[0].data.children[0].data.title,
+        bodyjp:
+          "LanguageApp.translate(body[0].data.children[0].data.title, `en`, `ja`)",
+        selftext: body[0].data.children[0].data.selftext,
+        selftestjp:
+          "LanguageApp.translate(body[0].data.children[0].data.selftext, `en`, `ja`)",
+        author: body[0].data.children[0].data.author,
+        id: body[0].data.children[0].data.id,
+        url: body[0].data.children[0].data.url,
+      };
 
-    //タイトルの取得、selfは投稿者のコメント
-    let title = {
-      body: body[0].data.children[0].data.title,
-      bodyjp : "LanguageApp.translate(body[0].data.children[0].data.title, `en`, `ja`)",
-      selftext : body[0].data.children[0].data.selftext,
-      selftestjp : "LanguageApp.translate(body[0].data.children[0].data.selftext, `en`, `ja`)",
-      author: body[0].data.children[0].data.author,
-      id: body[0].data.children[0].data.id,
-      url: body[0].data.children[0].data.url,
-    };
+      let comments = []; //コメント
 
-    let comments = [];//コメント
+      //コメントbodyとrepliesを取得
+      body[1].data.children.forEach((child) => {
+        extractCommentsAndReplies(child, comments);
+      });
 
-    //コメントbodyとrepliesを取得
-    body[1].data.children.forEach((child) => {
-      extractCommentsAndReplies(child, comments);
-    });
-
-    out = {
-      title: title,
-      comments: comments
-    }
-
-  })
-  .catch(error => console.error('There was a problem with your axios operation:', error));  
+      out = {
+        title: title,
+        comments: comments,
+      };
+    })
+    .catch((error) =>
+      console.error("There was a problem with your axios operation:", error)
+    );
 
   return out;
-
 }
 
 const extractCommentsAndReplies = (child, comments) => {
@@ -65,20 +71,19 @@ const extractCommentsAndReplies = (child, comments) => {
 
   comments.push({
     body: child.data.body,
-    bodyjp : "LanguageApp.translate(child.data.body, `en`, `ja`)",
+    bodyjp: "LanguageApp.translate(child.data.body, `en`, `ja`)",
     author: child.data.author,
     id: child.data.id,
     parent_id: child.data.parent_id,
   });
 
-  if(child.data.replies != ""){
+  if (child.data.replies != "") {
     //再帰関数で処理
     child.data.replies.data.children.forEach((child) => {
       extractCommentsAndReplies(child, comments);
     });
   }
-}
-
+};
 </script>
 
 <template>
@@ -91,7 +96,7 @@ const extractCommentsAndReplies = (child, comments) => {
 </template>
 
 <style scoped>
-  .template{
-    background-color: aquamarine;
-  }
+.template {
+  background-color: aquamarine;
+}
 </style>
